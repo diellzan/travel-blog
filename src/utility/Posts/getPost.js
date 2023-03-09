a0d5214ba1b2b6d9644310d49568216337fee54f
FlakeC <96479161+FlakeC@users.noreply.github.com>
2023-03-09 21:11:43 +0100

    Add files via upload

---
+++ src/utility/Posts/getPost.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const getPost = async (PostId) => {
+    const apiCaller = axios.create({
+        baseURL: "http://localhost:1000/",
+    });
+    const { data } = await apiCaller.get('/posts/${PostId}', {});
+    return data;
+};
+export default getPost;
