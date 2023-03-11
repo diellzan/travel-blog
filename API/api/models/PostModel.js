420d3ab2030b4d67c778e330cb88526d3ada53a0
diellzane <94406698+diellzan@users.noreply.github.com>
2023-03-11 16:05:00 +0100

    models

---
+++ API/api/models/PostModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const schema = mongoose.Schema({
+    blogTitle: String,
+    imgPAth: String,
+    blogHTML: String,
+    files: String,
+    insertedDate: Date,
+    updatedDate: Date,
+});
+
+const model = mongoose.model("Posts", schema);
+export default model;
